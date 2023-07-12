bf1dab2acd61d20a86894de976583172fa3d1929
Emil Knudstrup <emil@phys.au.dk>
2023-07-12 23:29:15 +0200

    git-lfs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,3 +99,11 @@ sphinxcontrib.bibtex.plugin.register_plugin(
    
 bibtex_reference_style = 'author_year_round'
 
+# Workaround to install and execute git-lfs on Read the Docs
+import os
+if not os.path.exists('./git-lfs'):
+    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('./git-lfs install')  # make lfs available in current repository
+    os.system('./git-lfs fetch')  # download content from remote
+    os.system('./git-lfs checkout')  # make local files to have the real content on them
